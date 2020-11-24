9f36be9f7bb14ee56181b424a249fcee605d59fb
Will Stone <654103+will-stone@users.noreply.github.com>
2020-11-24 17:13:59 +0000

    Better logging

--- demo/components/page.jsx
+++ demo/components/page.jsx
@@ -2,6 +2,7 @@ export const Page = ({ children }) => (
   <html lang="en">
     <head>
       <meta charSet="utf-8" />
+      <title>Page Title</title>
     </head>
     <body className="container">{children}</body>
   </html>
