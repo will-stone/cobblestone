e87859eaadd0059a2caed826cc097316eadb4657
Will Stone <654103+will-stone@users.noreply.github.com>
2020-12-02 09:08:44 +0000

    Refactor

--- demo/pages/index.jsx
+++ demo/pages/index.jsx
@@ -1,28 +1,26 @@
 import { Page } from '../components/page'
 import { PageTitle } from '../components/page-title'
 
-const Index = () => {
-  return (
-    <Page>
-      <header>
-        <img alt="" className="w-20" src="/avatar.png" />
-      </header>
-      <PageTitle>Index</PageTitle>
-      <nav>
-        <a className="underline" href="/about">
-          About
-        </a>
-      </nav>
-      <p className="text-gray-500">gray</p>
-      <p className="text-red-500">red</p>
-      <p className="text-blue-500">blue</p>
-      <p className="text-orange-500">orange</p>
-      <p className="text-yellow-500">yellow</p>
-      <p className="text-pink-500">pink</p>
-      <p className="text-teal-500">teal</p>
-      <p className="text-green-500">green</p>
-    </Page>
-  )
-}
+const Index = () => (
+  <Page>
+    <header>
+      <img alt="" className="w-20" src="/avatar.png" />
+    </header>
+    <PageTitle>Index</PageTitle>
+    <nav>
+      <a className="underline" href="/about">
+        About
+      </a>
+    </nav>
+    <p className="text-gray-500">gray</p>
+    <p className="text-red-500">red</p>
+    <p className="text-blue-500">blue</p>
+    <p className="text-orange-500">orange</p>
+    <p className="text-yellow-500">yellow</p>
+    <p className="text-pink-500">pink</p>
+    <p className="text-teal-500">teal</p>
+    <p className="text-green-500">green</p>
+  </Page>
+)
 
 export default Index
