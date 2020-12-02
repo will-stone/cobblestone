891038781c1b6c6363aa6edfe0d3a3f8c0fdd7b4
Will Stone <654103+will-stone@users.noreply.github.com>
2020-12-02 07:42:52 +0000

    Log watch change

--- demo/components/page.jsx
+++ demo/components/page.jsx
@@ -2,7 +2,7 @@ export const Page = ({ children }) => (
   <html lang="en">
     <head>
       <meta charSet="utf-8" />
-      <title>Page Title</title>
+      <title>Cobblestone</title>
     </head>
     <body className="container">{children}</body>
   </html>
